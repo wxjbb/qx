58620e4e27749a51965f72f29fcddb38fe257cef
xjbb <39824657+wxjbb@users.noreply.github.com>
2021-01-12 15:17:41 +0800

    Update gym.js

--- gym.js
+++ gym.js
@@ -1,9 +1,6 @@
-const body = $response.body;
-
-let obj = JSON.parse(body);
-
+let obj = JSON.parse($response.body);
 obj.data.name = "xjbb";
-
+obj.data.avatar = "b6d42043981f10fb0cfdc440a393ffa7";
 $done({
     body: JSON.stringify(obj)
 });
